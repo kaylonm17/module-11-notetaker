01e5a9bd41f1c1600cc17d6bd04aa40370e38a0e
Kaylon Myers <kaylonmyers2001@gmail.com>
2023-03-10 22:29:43 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+// Creates necessary Dependencies.
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
